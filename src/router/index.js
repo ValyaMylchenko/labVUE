27dbed2f5a07e8b2b08823987a8145a28b4483f0
Лилия Туманова ИА-83 <51443222+LiliaTumanova@users.noreply.github.com>
2022-04-22 13:27:23 +0300

    v4.0

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/Main'
+import Main from '@/components/Main'
 import First from '@/components/First'
 
 Vue.use(Router)
